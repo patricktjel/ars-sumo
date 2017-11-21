8e915ed467fb0e74dd7b37dd406f36f8e7df28b5
Lars Meijer <lars_meijer@outlook.com>
2017-11-21 11:34:00 +0100

    Added another learning agent, updated environment

--- ars_highway/agents/dqnSumo.py
+++ ars_highway/agents/dqnSumo.py
@@ -5,10 +5,10 @@ from keras.models import Sequential
 from keras.optimizers import Adam
 from rl.agents.dqn import DQNAgent
 from rl.memory import SequentialMemory
-from rl.policy import EpsGreedyQPolicy
+from rl.policy import BoltzmannQPolicy
 import environment
 
-ENV_NAME = 'MyEnv-v0'
+ENV_NAME = 'SumoEnv-v0'
 
 # Get the environment and extract the number of actions.
 env = gym.make(ENV_NAME)
@@ -32,7 +32,7 @@ print(model.summary())
 # Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
 # even the metrics!
 memory = SequentialMemory(limit=50000, window_length=1)
-policy = EpsGreedyQPolicy()
+policy = BoltzmannQPolicy()
 dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=50000,
                target_model_update=1e-2, policy=policy)
 dqn.compile(Adam(lr=1e-3), metrics=['mae'])
@@ -43,7 +43,7 @@ dqn.compile(Adam(lr=1e-3), metrics=['mae'])
 dqn.fit(env, nb_steps=50000, visualize=True, verbose=2)
 
 # After training is done, we save the final weights.
-dqn.save_weights('dqn_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
+#dqn.save_weights('dqn_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
 
 # Finally, evaluate our algorithm for 5 episodes.
 dqn.test(env, nb_episodes=5, visualize=True)
--- ars_highway/environment/__init__.py
+++ ars_highway/environment/__init__.py
@@ -1,6 +1,6 @@
 from gym.envs.registration import register
 
 register(
-    id='MyEnv-v0',
+    id='SumoEnv-v0',
     entry_point='environment.sumoEnv:SumoEnv',
 )
